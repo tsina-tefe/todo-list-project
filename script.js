c74c729d678d4578398d5c6cb26ede915e73ebb5
tsina-tefe <tsinukaltefera34@gmail.com>
2026-03-18 11:38:42 +0300

    Add event listeners for complete status and delte buttons

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const tasks = [
-    {
-        description: 'Push to github repo',
-        status: 'incomplete'
-    },
-    {
-        description: 'Push to github repo',
-        status: 'incomplete'
-    },
-    {
-        description: 'Push to github repo',
-        status: 'complete'
-    }
-];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 renderTasks();
+
+document.querySelector('.js-btn-add').addEventListener('click', () => {
+    addTask();
+});
+
+function saveToStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 function addTask() {
     const inputEle = document.querySelector('.js-task-input');
@@ -31,13 +26,15 @@ function addTask() {
         }, 2000);
     } else{
         tasks.push({
+            id: Date.now(),
             description: inputTask,
-            status: 'incomplete'
+            completed: false
         });
     }
 
     renderTasks();
     inputEle.value = '';
+    saveToStorage();
 }
 
 function renderTasks() {
@@ -46,16 +43,20 @@ function renderTasks() {
     tasks.forEach((task) => {
         html += 
         `
-            <div class="task-container">
+            <div class="task-container ${task.completed ? 'completed-blur' : ''}">
                 <div class="task">
-                    <button class="btn-complete">
-                        <img src="images/icons8-checkmark-48.png" alt="checkmark icon" width="20" height="20">
+                    <button class="btn-complete js-btn-complete" 
+                    data-task-id="${task.id}">
+                        ${task.completed 
+                            ? '<img src="images/icons8-checkmark-48.png" alt="checkmark icon" width="20" height="20">' 
+                            : ''}
                     </button>
-                    <p class="task-description">
+                    <p class="task-description ${task.completed ? 'mark-completed' : ''}">
                         ${task.description}
                     </p>
                 </div>
-                <button class="btn-delete">
+                <button class="btn-delete js-btn-delete"
+                data-task-id="${task.id}">
                     <img src="images/icons8-delete-24.png" alt="delete icon">
                 </button>
             </div>
@@ -64,17 +65,45 @@ function renderTasks() {
 
     document.querySelector('.task-list').innerHTML = html;
     renderRemaining();
-}
 
-document.querySelector('.js-btn-add').addEventListener('click', () => {
-    addTask();
-});
+    document.querySelectorAll('.js-btn-complete').forEach((btnComplete) => {
+        btnComplete.addEventListener('click', () => {
+            const {taskId} = btnComplete.dataset;
+            
+            tasks.forEach((task) => {
+                if(task.id === Number(taskId)) {
+                    task.completed = !task.completed;
+                }
+            });
+            
+            renderTasks();
+            saveToStorage();
+        });
+    });
+
+    document.querySelectorAll('.js-btn-delete').forEach((btnDelete) => {
+        btnDelete.addEventListener('click', () => {
+            const {taskId} = btnDelete.dataset;
+            let deleteIndex;
+
+            tasks.forEach((task, index) => {
+                if(task.id === Number(taskId)) {
+                    deleteIndex = index;
+                }
+            });
+
+            tasks.splice(deleteIndex, 1);
+            saveToStorage();
+            renderTasks();
+        });
+    });
+}
 
 function renderRemaining() {
     let remaining = 0;
 
     tasks.forEach((task) => {
-        if(task.status === 'incomplete') {
+        if(!task.completed) {
             remaining++;
         }
     });
